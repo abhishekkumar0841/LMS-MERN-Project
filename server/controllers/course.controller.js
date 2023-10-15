c45096e66282c1f219a691a1ee7e43bc7e5f8e67
abhishekkumar <abhishekkumar0841@gmail.com>
2023-10-15 16:44:01 +0530

    updateCourse controller done

--- server/controllers/course.controller.js
+++ server/controllers/course.controller.js
@@ -55,7 +55,7 @@ const createCourse = async function (req, res, next) {
       thumbnail: {
         public_id: "Thumbnail Public Id",
         secure_url: "Thumbnail Secure URL",
-      }
+      },
     });
 
     if (!course) {
@@ -95,7 +95,28 @@ const createCourse = async function (req, res, next) {
 };
 
 // *******UPDATE COURSE CONTROLLER******
-const updateCourse = async function (req, res, next) {};
+const updateCourse = async function (req, res, next) {
+  try {
+    const { id } = req.params;
+    const course = await Course.findByIdAndUpdate(
+      id,
+      { $set: req.body },//properties in req.body will replace the existing properties in the document
+      { runValidators: true } //it validate the data with comparing to courseSchema
+    );
+
+    if(!course){
+      return next(new AppError("Course with existing Id does not exist", 400))
+    }
+
+    res.status(200).json({
+      success: true,
+      message: "Course is updated successfully",
+      course
+    })
+  } catch (error) {
+    return next(new AppError(error.message, 500))
+  }
+};
 
 // *******REMOVE COURSE CONTROLLER******
 const removeCourse = async function (req, res, next) {};
