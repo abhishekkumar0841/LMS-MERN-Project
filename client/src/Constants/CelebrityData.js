c092fc0282968f795921b1f30abe111b03d469b7
abhishekkumar <abhishekkumar0841@gmail.com>
2023-10-17 21:10:41 +0530

    AboutUs page cleaned up

---
+++ client/src/Constants/CelebrityData.js
@@ -0,0 +1,43 @@
+import apj from "../assets/Images/apj.png";
+import billGates from "../assets/Images/billGates.png";
+import nelsonMandela from "../assets/Images/nelsonMandela.png";
+import einstein from "../assets/Images/einstein.png";
+import steveJobs from "../assets/Images/steveJobs.png";
+
+export const celebrities = [
+  {
+    title: "Nelson Mandela",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio libero sunt, doloribus delectus alias ab quos",
+    image: nelsonMandela,
+    slideNumber: 1,
+  },
+  {
+    title: "APJ Abdul Kalam",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio libero sunt, doloribus delectus alias ab quos",
+    image: apj,
+    slideNumber: 2,
+  },
+  {
+    title: "Albert Einstein",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio libero sunt, doloribus delectus alias ab quos",
+    image: einstein,
+    slideNumber: 3,
+  },
+  {
+    title: "Steve Jobs",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio libero sunt, doloribus delectus alias ab quos",
+    image: steveJobs,
+    slideNumber: 4,
+  },
+  {
+    title: "Bill Gates",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio libero sunt, doloribus delectus alias ab quos",
+    image: billGates,
+    slideNumber: 5,
+  },
+];
--- client/src/Pages/AboutUs.jsx
+++ client/src/Pages/AboutUs.jsx
@@ -1,13 +1,12 @@
 import React from "react";
 import HomeLayout from "../Layouts/HomeLayout";
 import aboutMainImage from "../assets/Images/aboutMainImage.png";
-import apj from "../assets/Images/apj.png";
-import billGates from "../assets/Images/billGates.png";
-import nelsonMandela from "../assets/Images/nelsonMandela.png";
-import einstein from "../assets/Images/einstein.png";
-import steveJobs from "../assets/Images/steveJobs.png";
+import CarouselSlide from "../Components/CarouselSlide";
+import { celebrities } from "../Constants/CelebrityData";
 
 const AboutUs = () => {
+  const totalSlides = celebrities.length;
+  
   return (
     <HomeLayout>
       <div className=" pl-20 pt-20 flex flex-col text-white">
@@ -37,111 +36,11 @@ const AboutUs = () => {
 
         {/* Carousal(about us page) */}
         <div className="carousel w-1/2 m-auto">
-          <div id="slide1" className="carousel-item relative w-full">
-            <div className="flex flex-col items-center justify-center gap-4 px-[15%]">
-              <img
-                src={nelsonMandela}
-                className="w-40 rounded-full border-2 border-green-400"
-              />
-              <p className=" text-xl text-gray-200 text-center">
-                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Rem,
-                esse eum? Magnam?
-              </p>
-              <h3 className=" text-2xl font-semibold">Nelson Mandela</h3>
-              <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-                <a href="#slide5" className="btn btn-circle">
-                  ❮
-                </a>
-                <a href="#slide2" className="btn btn-circle">
-                  ❯
-                </a>
-              </div>
-            </div>
-          </div>
-          <div id="slide2" className="carousel-item relative w-full">
-            <div className="flex flex-col items-center justify-center gap-4 px-[15%]">
-              <img
-                src={apj}
-                className="w-40 rounded-full border-2 border-green-400"
-              />
-              <p className=" text-xl text-gray-200 text-center">
-                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Rem,
-                esse eum? Magnam?
-              </p>
-              <h3 className=" text-2xl font-semibold">APJ Abdul Kalam</h3>
-              <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-                <a href="#slide1" className="btn btn-circle">
-                  ❮
-                </a>
-                <a href="#slide3" className="btn btn-circle">
-                  ❯
-                </a>
-              </div>
-            </div>
-          </div>
-          <div id="slide3" className="carousel-item relative w-full">
-            <div className="flex flex-col items-center justify-center gap-4 px-[15%]">
-              <img
-                src={einstein}
-                className="w-40 rounded-full border-2 border-green-400"
-              />
-              <p className=" text-xl text-gray-200 text-center">
-                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Rem,
-                esse eum? Magnam?
-              </p>
-              <h3 className=" text-2xl font-semibold">Einstein</h3>
-              <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-                <a href="#slide2" className="btn btn-circle">
-                  ❮
-                </a>
-                <a href="#slide4" className="btn btn-circle">
-                  ❯
-                </a>
-              </div>
-            </div>
-          </div>
-          <div id="slide4" className="carousel-item relative w-full">
-            <div className="flex flex-col items-center justify-center gap-4 px-[15%]">
-              <img
-                src={steveJobs}
-                className="w-40 rounded-full border-2 border-green-400"
-              />
-              <p className=" text-xl text-gray-200 text-center">
-                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Rem,
-                esse eum? Magnam?
-              </p>
-              <h3 className=" text-2xl font-semibold">Steve Jobs</h3>
-              <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-                <a href="#slide3" className="btn btn-circle">
-                  ❮
-                </a>
-                <a href="#slide5" className="btn btn-circle">
-                  ❯
-                </a>
-              </div>
-            </div>
-          </div>
-          <div id="slide5" className="carousel-item relative w-full">
-            <div className="flex flex-col items-center justify-center gap-4 px-[15%]">
-              <img
-                src={billGates}
-                className="w-40 rounded-full border-2 border-green-400"
-              />
-              <p className=" text-xl text-gray-200 text-center">
-                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Rem,
-                esse eum? Magnam?
-              </p>
-              <h3 className=" text-2xl font-semibold">Bill Gates</h3>
-              <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-                <a href="#slide4" className="btn btn-circle">
-                  ❮
-                </a>
-                <a href="#slide1" className="btn btn-circle">
-                  ❯
-                </a>
-              </div>
-            </div>
-          </div>
+          {
+            celebrities && celebrities.map((celebrity, index) => (
+              <CarouselSlide {...celebrity} key={index} totalSlides={totalSlides} />
+            ))
+          }
         </div>
       </div>
     </HomeLayout>
