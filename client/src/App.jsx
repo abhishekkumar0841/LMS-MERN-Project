be44cd6129ae038dfcb1de2e17cc1a1a95f9d496
abhishekkumar <abhishekkumar0841@gmail.com>
2023-10-19 12:39:28 +0530

    Denied Page added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,30 +1,34 @@
 import "./App.css";
 import { Route, Routes } from "react-router-dom";
-import HomePage from './Pages/HomePage'
+import HomePage from "./Pages/HomePage";
 import AboutUs from "./Pages/AboutUs";
 import NotFound from "./Components/NotFound";
 import Signup from "./Pages/Signup";
 import Login from "./Pages/Login";
 import CourseList from "./Pages/Course/CourseList";
 import Contact from "./Pages/Contact";
+import Denied from "./Pages/Denied";
 
 function App() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<HomePage/>} />
-        <Route path="/about" element={<AboutUs/>} />
-        <Route path="/signup" element={<Signup/>}/>
-        <Route path="/login" element={<Login/>}/>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/about" element={<AboutUs />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/login" element={<Login />} />
 
         {/* Course routes */}
-        <Route path="/courses" element={<CourseList/>}/>
+        <Route path="/courses" element={<CourseList />} />
 
-         {/* Contact page routes */}
-         <Route path="/contact" element={<Contact/>}/>
+        {/* Contact page routes */}
+        <Route path="/contact" element={<Contact />} />
+
+        {/* Denied page routes */}
+        <Route path="/denied" element={<Denied />} />
 
         {/* Error Page Route */}
-        <Route path="*" element={<NotFound/>}/>
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </>
   );
