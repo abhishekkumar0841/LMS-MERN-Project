7516fa0415eef1e9ce7c24ba2dbf4d5ed8fd4306
abhishekkumar <abhishekkumar0841@gmail.com>
2023-10-27 00:13:05 +0530

    rollback to prev token handling

--- server/middlewares/auth.middleware.js
+++ server/middlewares/auth.middleware.js
@@ -5,7 +5,7 @@ import asyncHandler from 'express-async-handler'
 
 // ********AUTHENTICATION MIDDLEWARE************
 const isLoggedIn = async (req, res, next) => {
-  const token  = req.cookies.token || req.header("Authorization").replace("Bearer ", "");
+  const { token } = req.cookies;
 
   if (!token) {
     return next(new AppError("Unauthenticated, please login again", 400));
